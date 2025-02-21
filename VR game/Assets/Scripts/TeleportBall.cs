938277028292c31d95b3cb0f4513e361db65149d
Oliver Saari <oliver.saari@student.oulu.fi>
2025-02-21 16:28:07 +0200

    Animations work, hand physics are limited, teleport to ball added

---
+++ VR game/Assets/Scripts/TeleportBall.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TeleportPlayerToBall : MonoBehaviour
+{
+    public InputActionReference teleportAction;
+    public Transform player;
+    public Transform ball;
+
+    void OnEnable()
+    {
+        teleportAction.action.Enable();
+        teleportAction.action.performed += OnTeleportActionPerformed;
+    }
+
+    void OnDisable()
+    {
+        teleportAction.action.performed -= OnTeleportActionPerformed;
+        teleportAction.action.Disable();
+    }
+
+    void OnTeleportActionPerformed(InputAction.CallbackContext context)
+    {
+        TeleportPlayerToBallPosition();
+    }
+
+    void TeleportPlayerToBallPosition()
+    {
+        player.position = ball.position + Vector3.back * 1f; // Teleporttaa pelaajan pallon yläpuolelle
+    }
+}
